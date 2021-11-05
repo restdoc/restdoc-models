93563e6672425e5b4b4d98f8cd486ddea175c339
solos <lxl1217@gmail.com>
2021-11-05 15:36:57 +0800

    add models
    add models

---
+++ models/maillog.go
@@ -0,0 +1,83 @@
+package Models
+
+import (
+	"fmt"
+
+	_ "github.com/go-sql-driver/mysql"
+)
+
+type MailLog struct {
+	ID         string `gorm:"type:varchar(100);not null;primary_key;" json:"id"`
+	UserId     uint64 `gorm:"type:bigint unsigned;not null;" json:"user_id"`
+	Domain     string `gorm:"type:varchar(128);not null;" json:"domain"`
+	Type       uint8  `gorm:"type:tinyint(8);not null;" json:"type"`
+	From       string `gorm:"type:varchar(128);not null;" json:"from"`
+	To         string `gorm:"type:varchar(128);not null;" json:"to"`
+	Subject    string `gorm:"type:varchar(255);not null;" json:"subject"`
+	Created    uint64 `gorm:"type:bigint unsigned;not null;" json:"created"`
+	Updated    uint64 `gorm:"type:bigint unsigned;not null;" json:"updated"`
+	Status     uint8  `gorm:"type:smallint unsigned;not null;" json:"status"`
+	Code       uint16 `gorm:"type:smallint unsigned;not null;" json:"code"`
+	Msg        string `gorm:"type:varchar(255);not null;" json:"msg"`
+	SmtpServer string `gorm:"type:varchar(128);not null;" json:"smtp_server"`
+}
+
+func (m *MailLog) TableName() string {
+	return "maillog"
+}
+
+func AddNewEmail(m *MailLog) (err error) {
+	if err = DB.Create(m).Error; err != nil {
+		return err
+	}
+	return nil
+}
+
+func GetOneEmail(m *MailLog, id string) (err error) {
+	if err := DB.Where("id = ?", id).First(m).Error; err != nil {
+		return err
+	}
+	return nil
+}
+
+func GetEmailByDomainName(m *MailLog, domain string) (err error) {
+	if err := DB.Where("domain = ?", domain).First(m).Error; err != nil {
+		return err
+	}
+	return nil
+}
+
+func GetEmailsByUserId(emails *[]MailLog, user_id uint64, limit int, offset int) (err error) {
+	if err := DB.Where("user_id = ?", user_id).Find(&emails).Error; err != nil {
+		fmt.Println(err)
+		return err
+	}
+	return nil
+}
+
+func GetEmailsByDomain(emails *[]MailLog, domain string, limit int, offset int) (err error) {
+	if err := DB.Where("domain = ?", domain).Find(&emails).Error; err != nil {
+		fmt.Println(err)
+		return err
+	}
+	return nil
+}
+
+func PutOneEmail(m *MailLog) (err error) {
+	DB.Save(m)
+	return nil
+}
+
+func UpdateStatus(m *MailLog) error {
+	if err := DB.Model(m).Where("id = ?", m.ID).Updates(MailLog{Status: m.Status, Code: m.Code, Msg: m.Msg, Updated: m.Updated}).Error; err != nil {
+		return err
+	}
+	return nil
+}
+
+func SummaryEmails(emails *[]MailLog, begin int64, end int64) (err error) {
+	if err := DB.Where("created > ? and created < ?", begin, end).Select("user_id, status, domain").Find(&emails).Error; err != nil {
+		return err
+	}
+	return nil
+}
---
+++ models/summary.go
@@ -0,0 +1,76 @@
+package Models
+
+import (
+	"fmt"
+
+	_ "github.com/go-sql-driver/mysql"
+)
+
+type Summary struct {
+	ID         uint64 `gorm:"type:bigint unsigned auto_increment; not null; primary_key;" json: "id"`
+	UserId     uint64 `gorm:"type:bigint unsigned;not null;unique_index:user_id_date_domain" json:"user_id"`
+	Date       string `gorm:"type:varchar(20); not null;unique_index:user_id_date_domain" json:"date"`
+	Domain     string `gorm:"type:varchar(128);not null;unique_index:user_id_date_domain" json:"domain"`
+	Processed  uint64 `gorm:"type:bigint unsigned;not null;" json:"processed"`
+	Delivered  uint64 `gorm:"type:bigint unsigned;not null;" json:"delivered"`
+	Suppressed uint64 `gorm:"type:bigint unsigned;not null;" json:"suppressed"` //软退信
+	Dropped    uint64 `gorm:"type:bigint unsigned;not null;" json:"dropped"`    //无效邮件
+	Spam       uint64 `gorm:"type:bigint unsigned;not null;" json:"spam"`       //垃圾邮件举报
+}
+
+func (s *Summary) TableName() string {
+	return "summary"
+}
+
+func AddNewSummary(s *Summary) (err error) {
+	if err = DB.Create(s).Error; err != nil {
+		return err
+	}
+	return nil
+}
+
+func GetOneSummary(s *Summary, id string) (err error) {
+	if err := DB.Where("id = ?", id).First(s).Error; err != nil {
+		return err
+	}
+	return nil
+}
+
+func GetSummaryByDomainName(s *Summary, domain string) (err error) {
+	if err := DB.Where("domain = ?", domain).First(s).Error; err != nil {
+		return err
+	}
+	return nil
+}
+
+func GetSummariesByUserId(summaries *[]Summary, user_id uint64) (err error) {
+	if err := DB.Where("user_id = ?", user_id).Find(&summaries).Error; err != nil {
+		fmt.Println(err)
+		return err
+	}
+	return nil
+}
+
+func GetSummariesByDomain(summaries *[]Summary, domain string, limit int, offset int) (err error) {
+	if err := DB.Where("domain = ?", domain).Find(&summaries).Error; err != nil {
+		fmt.Println(err)
+		return err
+	}
+	return nil
+}
+
+func PutOneSummary(s *Summary) (err error) {
+	DB.Save(s)
+	return nil
+}
+
+func SaveSummary(s *Summary) error {
+
+	//db.Where(User{Name: "non_existing"}).Attrs(User{Age: 20}).FirstOrCreate(&user)
+
+	rows := DB.Model(s).Where("user_id = ? and date = ? and domain = ?", s.UserId, s.Date, s.Domain).Updates(Summary{Processed: s.Processed, Delivered: s.Delivered, Suppressed: s.Suppressed, Dropped: s.Dropped, Spam: s.Spam}).RowsAffected
+	if rows == 0 {
+		DB.Save(s)
+	}
+	return nil
+}
